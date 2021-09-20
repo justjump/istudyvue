c547a8b54791d1e81b39abdac3adecfb3472dd00
justjump <1976570@qq.com>
2021-09-21 00:25:59 +0800

    第一次用webpack打包成功

---
+++ src/assets/js/ccc.js
@@ -0,0 +1,5 @@
+var c1="正方形";
+export var c2="三角形";
+var c3="椭圆形";
+
+export {c1};
---
+++ src/assets/js/main.js
@@ -0,0 +1,14 @@
+import {flag,say,todo} from './aaa.js';
+console.log(flag)
+say();
+todo();
+
+import b from './bbb.js';
+console.log(b);
+b.daikuan(10,.2,15);
+
+import * as c from './ccc.js';
+console.log(c);
+
+//修改一下这个文件,看看git有什么反应?
+//用git status来跟踪.
