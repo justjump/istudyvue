d5cc2442ade4743717fe4723b96737a868924966
justjump <1976570@qq.com>
2021-09-21 20:10:50 +0800

    webpack学习,复习组件化

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,14 +1,33 @@
-import {flag,say,todo} from './aaa.js';
+import {
+    flag,
+    say,
+    todo
+} from './aaa.js';
 console.log(flag)
 say();
 todo();
 
 import b from './bbb.js';
 console.log(b);
-b.daikuan(10,.2,15);
+b.daikuan(10, .2, 15);
 
 import * as c from './ccc.js';
 console.log(c);
 
+
+//引入vue
+import Vue from 'vue';
+console.log(Vue)
+new Vue({
+    el: "#app1",
+    template:`
+        <h1>{{myname}}</h1>
+    `,
+    data: {
+        myname: 'hehehe'
+    }
+})
+// import Vue from "/node_modules/vue/dist/vue.js";
 //修改一下这个文件,看看git有什么反应?
 //用git status来跟踪.
+require("../css/normal.css");
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,27 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index.js',
+    },
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ],
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js' // 用 webpack 1 时需用 'vue/dist/vue.common.js'
+        }
     }
 }
